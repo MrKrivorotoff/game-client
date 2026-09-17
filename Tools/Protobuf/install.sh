f48500c2bfc2c1c350a851e6794ec5f7705dd89c
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2026-09-18 00:39:06 +0300

    Update protobuf version

--- Tools/Protobuf/install.sh
+++ Tools/Protobuf/install.sh
@@ -4,11 +4,11 @@ set -e
 cd "$(dirname "$0")"
 
 curl -L \
-  https://github.com/protocolbuffers/protobuf/releases/download/v36.1/protoc-36.1-win64.zip \
+  https://github.com/protocolbuffers/protobuf/releases/download/v36.2/protoc-36.2-win64.zip \
   -o protoc.zip
 
 rm -rf Protoc
 unzip -q protoc.zip -d Protoc
 rm protoc.zip
 
-echo "protoc 36.1 installed"
+echo "protoc 36.2 installed"
